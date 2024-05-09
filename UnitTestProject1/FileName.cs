592906570b47ff85dff6ee0b6d80967938f2be8e
Alvarez <72086457@continental.edu.pe>
2024-05-08 23:14:28 -0500

    Agregar archivos de proyecto.

---
+++ UnitTestProject1/FileName.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PROYECTO_MCD
+namespace UnitTestProject1
+{
+    public class FileName
+    {
+        Calculadora calculadora = new Calculadora();
+        int nro1 = 300;
+        int nro2 = 33800;
+        int esperado = 20;
+
+        // Act
+        int actual = calculadora.CalcularMCD(nro1, nro2);
+
+        // Assert
+        Assert.AreEqual(esperado, actual);
+    }
+}
